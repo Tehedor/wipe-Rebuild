96fc2afb1249d6ae265f8bd7ac1b5c63c5caca60
Sergio <sergiot1805@gmail.com>
2024-12-03 10:39:47 +0100

    a

--- scritpII/1.1_pruebas_volumes_dir_mongo/export_volume.sh
+++ scritpII/1.1_pruebas_volumes_dir_mongo/export_volume.sh
@@ -23,9 +23,16 @@ MONGO_NODE=$(kubectl get pod $MONGO_POD --all-namespaces -o jsonpath='{.spec.nod
 # Mostrar el nodo en el que está montado el pod de MongoDB
 echo "El pod de MongoDB '$MONGO_POD' está montado en el nodo '$MONGO_NODE'."
 
+# Definir las rutas
+SOURCE_DIR="/mnt/mongo"
+DEST_DIR=~/Documents/1_snapshotVolumes/export
+
+# Crear el directorio de destino si no existe
+mkdir -p $DEST_DIR
 
 # Exportar el VolumeDirectory
-scp -r develop@$MONGO_NODE:/mnt/mongo /Docuemnts/1_snapshotVolumes/export
+scp -i ~/.ssh/kmaster01 -r develop@$MONGO_NODE:$SOURCE_DIR $DEST_DIR
+
 echo "Exportación completada. Archivos guardados en: $EXPORT_DIR"
 
 # # Exportar el PVC
