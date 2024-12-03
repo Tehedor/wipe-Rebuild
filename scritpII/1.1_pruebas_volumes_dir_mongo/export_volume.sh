5c18d2f48d0059237689e6e886148a3bfdc71a20
Sergio <sergiot1805@gmail.com>
2024-12-03 10:11:54 +0100

    a

--- scritpII/1.1_pruebas_volumes_dir_mongo/export_volume.sh
+++ scritpII/1.1_pruebas_volumes_dir_mongo/export_volume.sh
@@ -10,13 +10,13 @@ mkdir -p $EXPORT_DIR
 # Exportar el VolumeDirectory
 scp -r develop@kworker01.lab.local:/mnt/mongo /Docuemnts/1_snapshotVolumes/export
 
-# Exportar el PVC
-kubectl get pvc $VOLUME_NAME -o yaml > $EXPORT_DIR/$VOLUME_NAME.yaml
+# # Exportar el PVC
+# kubectl get pvc $VOLUME_NAME -o yaml > $EXPORT_DIR/$VOLUME_NAME.yaml
 
-# Exportar el PV asociado al PVC
-PV_NAME=$(kubectl get pvc $VOLUME_NAME -o jsonpath='{.spec.volumeName}')
-kubectl get pv $PV_NAME -o yaml > $EXPORT_DIR/$PV_NAME.yaml
+# # Exportar el PV asociado al PVC
+# PV_NAME=$(kubectl get pvc $VOLUME_NAME -o jsonpath='{.spec.volumeName}')
+# kubectl get pv $PV_NAME -o yaml > $EXPORT_DIR/$PV_NAME.yaml
 
-echo "Exportación completada. Archivos guardados en: $EXPORT_DIR"
+# echo "Exportación completada. Archivos guardados en: $EXPORT_DIR"
 
 # bash ./delete_volumes_mongo.sh 
