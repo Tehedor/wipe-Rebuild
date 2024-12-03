ed218e45ecab3fa0740e22749d865218070622f3
Sergio <sergiot1805@gmail.com>
2024-12-03 11:07:08 +0100

    a

--- scritpII/1.1_pruebas_volumes_dir_mongo/export_volume.sh
+++ scritpII/1.1_pruebas_volumes_dir_mongo/export_volume.sh
@@ -7,9 +7,9 @@ VOLUME_NAME="mongo-pvc"
 EXPORT_DIR=~/Documents/1_snapshotVolumes/export
 mkdir -p $EXPORT_DIR
 
-# Quiero que me saque en que nodo esta montado el pod de mongo
-# Obtener el nombre del pod de MongoDB
+# Obtener el nombre del pod de MongoDB y su namespace
 MONGO_POD=$(kubectl get pods --all-namespaces -o jsonpath='{.items[?(@.metadata.labels.app=="mongo")].metadata.name}')
+MONGO_NAMESPACE=$(kubectl get pods --all-namespaces -o jsonpath='{.items[?(@.metadata.labels.app=="mongo")].metadata.namespace}')
 
 # Verificar si se encontró el pod de MongoDB
 if [ -z "$MONGO_POD" ]; then
@@ -18,7 +18,13 @@ if [ -z "$MONGO_POD" ]; then
 fi
 
 # Obtener el nodo en el que está montado el pod de MongoDB
-MONGO_NODE=$(kubectl get pod $MONGO_POD --all-namespaces -o jsonpath='{.spec.nodeName}')
+MONGO_NODE=$(kubectl get pod $MONGO_POD -n $MONGO_NAMESPACE -o jsonpath='{.spec.nodeName}')
+
+# Verificar si se encontró el nodo
+if [ -z "$MONGO_NODE" ]; then
+    echo "No se encontró el nodo en el que está montado el pod de MongoDB."
+    exit 1
+fi
 
 # Mostrar el nodo en el que está montado el pod de MongoDB
 echo "El pod de MongoDB '$MONGO_POD' está montado en el nodo '$MONGO_NODE'."
