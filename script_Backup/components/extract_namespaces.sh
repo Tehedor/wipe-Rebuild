663a288ec7cd60a99f9b0755674890a7af00a411
tehedor <sergiot1805@gmail.com>
2024-12-17 12:32:49 +0100

    backups

---
+++ script_Backup/components/extract_namespaces.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+extract_namespace(){
+    echo "Extracting namespaces"
+    kubectl get namespaces
+}
---
+++ script_Backup/general.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+source "./components/*"
+
+# 1.- Detectar todos los namespaces exitentes
+
+
+# 2.- Dividir Namespace por servicio
+extract_namespace
+
+
+    # 2.1.- Sacaremos de cada servicio todo los resource types que haya   
+
+
+    # 2.2- Ver si hay volumne montado
+
+        # Si hay volumen lo compimiremos en un tar.gz
+
+    
+    # 2.3- Filtraremos del file de config lo recursos necesarioa para el backup
+
+
+
+    # 2.3- Exportar resultado de 2.2 y 2.3 al bucket de backup
+
+
+
+# 
